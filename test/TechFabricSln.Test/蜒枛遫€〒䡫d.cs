c9e22e0062ec74d0b0ae3dedcc832f8ca3c95924
cti <cti@test.com>
2022-09-13 17:03:41 +0000

    更新于 蜒枛遫€〒䡫d.cs

--- test/TechFabricSln.Test/蜒枛遫€〒䡫d.cs
+++ test/TechFabricSln.Test/蜒枛遫€〒䡫d.cs
@@ -1,6 +1,4 @@
 using NUnit.Framework;
-
-namespace TechFabricSln.Test
 {
     public class Tests
     {
@@ -11,20 +9,26 @@ namespace TechFabricSln.Test
 
         [Test]
         [Category("UnitTest")]
-        public void Test1()
+        public void 测试方法啊阿鼾齄丂丄狚狛狜狝﨨()
         {
             Assert.Pass();
         }
 
         [Test]
         [Category("UnitTest")]
-        public void IsBoughtBy_UserIsCustomer_ReturnTrue()
+        public void 测试方法癌蔼矮艾碍爱隘鼾D2丠両狛丣囆囇囈囉()
         {
             var bought = new Bought();
 
             var result = bought.IsBoughtBy(new User { customer = true });
 
             Assert.IsTrue(result);
+        }
+        [Test]
+        [Category("UnitTest")]
+        public void 测试方法舰剑饯渐溅涧建僵齄D2囌囍囎囏囐囓囑囒D3珸珹䲟珺珻珼珽現()
+        {
+            Assert.Pass();
         }
     }
 }
