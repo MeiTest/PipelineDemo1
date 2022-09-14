fb5b674b4c5ed366cc5554e75ed9c5b22dabcfc3
cti <cti@test.com>
2022-09-14 02:36:09 +0000

    更新于 UnitTests.cs

--- test/TechFabricSln.Test/UnitTests.cs
+++ test/TechFabricSln.Test/UnitTests.cs
@@ -7,26 +7,32 @@ using NUnit.Framework;
         {
         }
 
-        [Test]
-        [Category("UnitTest")]
-        public void 测试方法啊阿鼾齄丂丄狚狛狜狝﨨()
-        {
-            Assert.Pass();
-        }
+        // [Test]
+        // [Category("UnitTest")]
+        // public void 测试方法啊阿鼾齄丂丄狚狛狜狝﨨()
+        // {
+        //     Assert.Pass();
+        // }
 
-        [Test]
-        [Category("UnitTest")]
-        public void 测试方法癌蔼矮艾碍爱隘鼾D2丠両狛丣囆囇囈囉()
-        {
-            var bought = new Bought();
+        // [Test]
+        // [Category("UnitTest")]
+        // public void 测试方法癌蔼矮艾碍爱隘鼾D2丠両狛丣囆囇囈囉()
+        // {
+        //     var bought = new Bought();
 
-            var result = bought.IsBoughtBy(new User { customer = true });
+        //     var result = bought.IsBoughtBy(new User { customer = true });
 
-            Assert.IsTrue(result);
-        }
-        [Test]
+        //     Assert.IsTrue(result);
+        // }
+        // [Test]
+        // [Category("UnitTest")]
+        // public void 测试方法舰剑饯渐溅涧建僵齄D2囌囍囎囏囐囓囑囒D3珸珹䲟珺珻珼珽現()
+        // {
+        //     Assert.Pass();
+        // }
+                [Test]
         [Category("UnitTest")]
-        public void 测试方法舰剑饯渐溅涧建僵齄D2囌囍囎囏囐囓囑囒D3珸珹䲟珺珻珼珽現()
+        public void test()
         {
             Assert.Pass();
         }
