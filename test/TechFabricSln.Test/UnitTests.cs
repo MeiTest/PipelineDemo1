5fcb49734eadf69227af46e5e7e80cbdde91577b
cti <cti@test.com>
2022-09-14 02:51:28 +0000

    更新于 UnitTests.cs

--- test/TechFabricSln.Test/UnitTests.cs
+++ test/TechFabricSln.Test/UnitTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+namespace TechFabricSln.Test
 {
     public class Tests
     {
@@ -7,34 +8,29 @@ using NUnit.Framework;
         {
         }
 
-        // [Test]
-        // [Category("UnitTest")]
-        // public void 测试方法啊阿鼾齄丂丄狚狛狜狝﨨()
-        // {
-        //     Assert.Pass();
-        // }
-
-        // [Test]
-        // [Category("UnitTest")]
-        // public void 测试方法癌蔼矮艾碍爱隘鼾D2丠両狛丣囆囇囈囉()
-        // {
-        //     var bought = new Bought();
-
-        //     var result = bought.IsBoughtBy(new User { customer = true });
-
-        //     Assert.IsTrue(result);
-        // }
-        // [Test]
-        // [Category("UnitTest")]
-        // public void 测试方法舰剑饯渐溅涧建僵齄D2囌囍囎囏囐囓囑囒D3珸珹䲟珺珻珼珽現()
-        // {
-        //     Assert.Pass();
-        // }
-                [Test]
+        [Test]
         [Category("UnitTest")]
-        public void test()
+        public void 测试方法啊阿鼾齄丂丄狚狛狜狝﨨()
         {
             Assert.Pass();
         }
+
+        [Test]
+        [Category("UnitTest")]
+        public void 测试方法癌蔼矮艾碍爱隘鼾D2丠両狛丣囆囇囈囉()
+        {
+            var bought = new Bought();
+
+            var result = bought.IsBoughtBy(new User { customer = true });
+
+            Assert.IsTrue(result);
+        }
+        [Test]
+        [Category("UnitTest")]
+        public void 测试方法舰剑饯渐溅涧建僵齄D2囌囍囎囏囐囓囑囒D3珸珹䲟珺珻珼珽現()
+        {
+            Assert.Pass();
+        }
+
     }
 }
